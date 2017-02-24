07028b3613f3c8c3d532d36b2c7e7a216338136f
Minhoko <minoerick120@gmail.com>
2017-02-24 15:23:57 +0900

    update at 3pm feb-24

--- client-side/src/components/Ground.js
+++ client-side/src/components/Ground.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Popover, PopoverTitle, PopoverContent } from 'reactstrap';
+import { Container, Row, Col, Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 const propTypes = {
   allDisplayTeam: React.PropTypes.object,
@@ -12,15 +12,24 @@ const defaultProps = {
 class Ground extends Component {
   constructor(props) {
     super(props);
-    this.toggle = this.toggle.bind(this);
     this.state = {
-      popoverOpen: false,
+      modal: false,
+      member: '',
     };
+    this.toggle = this.toggle.bind(this);
+    this.onMemberModal = this.onMemberModal.bind(this);
+  }
+
+  onMemberModal(member) {
+    this.setState({
+      modal: !this.state.modal,
+      member,
+    });
   }
 
   toggle() {
     this.setState({
-      popoverOpen: !this.state.popoverOpen,
+      modal: !this.state.modal,
     });
   }
 
@@ -40,23 +49,20 @@ class Ground extends Component {
     const homeGK = homeUsers.find(homeUser => homeUser.position === 'GK');
     const homeFW = homeUsers.find(homeUser => homeUser.position === 'FW');
     const homeMF = homeUsers.find(homeUser => homeUser.position === 'MF');
-    const homeDFs = homeUsers.reduce((result, homeUser) => {
-      if (homeUser.position === 'DF' && result.length < 2) {
-        result.push(homeUser);
-      }
-      return result;
-    }, []);
+    const homeDF1 = homeUsers.find(homeUser => homeUser.position === 'DF');
+    const homeDF2 = homeUsers.find(homeUser => (homeUser.position === 'DF') && (homeDF1.username !== homeUser.username));
     const homeSubs = homeUsers.filter((homeUser) => {
       return ![
         homeGK,
         homeFW,
         homeMF,
-        ...homeDFs,
+        homeDF1,
+        homeDF2,
       ].includes(homeUser);
     });
     const homeSubMembers = (
       members => members.map(member =>
-        <div className="player-sub">
+        <div className="player-sub" key={member.id}>
           <div
             className="homesub-player"
             style={{ backgroundImage: `url(${member.userImgUrl})` }}
@@ -71,23 +77,20 @@ class Ground extends Component {
     const awayGK = awayUsers.find(awayUser => awayUser.position === 'GK');
     const awayFW = awayUsers.find(awayUser => awayUser.position === 'FW');
     const awayMF = awayUsers.find(awayUser => awayUser.position === 'MF');
-    const awayDFs = awayUsers.reduce((result, awayUser) => {
-      if (awayUser.position === 'DF' && result.length < 2) {
-        result.push(awayUser);
-      }
-      return result;
-    }, []);
+    const awayDF1 = awayUsers.find(awayUser => awayUser.position === 'DF');
+    const awayDF2 = awayUsers.find(awayUser => (awayUser.position === 'DF') && (awayDF1.username !== awayUser.username));
     const awaySubs = awayUsers.filter((awayUser) => {
       return ![
         awayGK,
         awayFW,
         awayMF,
-        ...awayDFs,
+        awayDF1,
+        awayDF2,
       ].includes(awayUser);
     });
     const awaySubMembers = (
       members => members.map(member =>
-        <div className="player-sub">
+        <div className="player-sub" key={member.id}>
           <div
             className="awaysub-player"
             style={{ backgroundImage: `url(${member.userImgUrl})` }}
@@ -97,29 +100,18 @@ class Ground extends Component {
       )
     );
 
-
-/* AWAY TEAM POPOVER SECTION */
-  const awayPlayer1 = (
-    <Popover
-      placement="bottom"
-      isOpen={this.state.popoverOpen}
-      target="homeGK"
-      toggle={this.toggle}
-    >
-      <PopoverTitle>Hometeam GK</PopoverTitle>
-      <PopoverContent>Hello</PopoverContent>
-    </Popover>
-  );
-
+    const totalMembers = [
+      homeGK, homeFW, homeDF1, homeDF2, homeMF, ...homeSubs, awayGK, awayFW, awayDF1, awayDF2, awayMF, ...awaySubs,
+    ];
 
   /* AWAY TEAM DETAILED INFO */
-    const awayTeamName = awayGK ? awayGK.team : false;
+    const awayTeamName = awayFW ? awayFW.team : false;
     const awayGKImage = awayGK ? awayGK.userImgUrl : '';
     const awayGKUsername = awayGK ? awayGK.username : false;
-    const awayDFs0Imgge = awayDFs[0] ? awayDFs[0].userImgUrl : '';
-    const awayDFs0Username = awayDFs && awayDFs[0] ? awayDFs[0].username : false;
-    const awayDFs1Imgge = awayDFs[1] ? awayDFs[1].userImgUrl : '';
-    const awayDFs1Username = awayDFs && awayDFs[1] ? awayDFs[1].username : false;
+    const awayDF1Imgge = awayDF1 ? awayDF1.userImgUrl : '';
+    const awayDF1Username = awayDF1 ? awayDF1.username : false;
+    const awayDF2Imgge = awayDF2 ? awayDF2.userImgUrl : '';
+    const awayDF2Username = awayDF2 ? awayDF2.username : false;
     const awayMFImage = awayMF ? awayMF.userImgUrl : '';
     const awayMFUsername = awayMF ? awayMF.username : false;
     const awayFWImage = awayFW ? awayFW.userImgUrl : '';
@@ -127,17 +119,68 @@ class Ground extends Component {
 
 
     /* HOME TEAM DETAILED INFO */
-    const homeTeamName = homeGK ? homeGK.team : false;
+    const homeTeamName = homeFW ? homeFW.team : false;
     const homeGKImage = homeGK ? homeGK.userImgUrl : '';
     const homeGKUsername = homeGK ? homeGK.username : false;
-    const homeDFs0Imgge = homeDFs[0] ? homeDFs[0].userImgUrl : '';
-    const homeDFs0Username = homeDFs && homeDFs[0] ? homeDFs[0].username : false;
-    const homeDFs1Imgge = homeDFs[1] ? homeDFs[1].userImgUrl : '';
-    const homeDFs1Username = homeDFs && homeDFs[1] ? homeDFs[1].username : false;
+    const homeDF1Imgge = homeDF1 ? homeDF1.userImgUrl : '';
+    const homeDF1Username = homeDF1 ? homeDF1.username : false;
+    const homeDF2Imgge = homeDF2 ? homeDF2.userImgUrl : '';
+    const homeDF2Username = homeDF2 ? homeDF2.username : false;
     const homeMFImage = homeMF ? homeMF.userImgUrl : '';
     const homeMFUsername = homeMF ? homeMF.username : false;
     const homeFWImage = homeFW ? homeFW.userImgUrl : '';
     const homeFWUsername = homeFW ? homeFW.username : false;
+
+    /* MODAL SECTION */
+    const playerModal = (
+      (member) => {
+        const modalFilter = totalMembers.find(
+          modalUser => modalUser ? modalUser.username === member : false
+        );
+        if (member) {
+          return (
+            <Modal isOpen={this.state.modal} toggle={this.toggle} className="modal-body">
+              <ModalHeader toggle={this.toggle} className="modal-head">
+                선수 상세정보
+              </ModalHeader>
+              <ModalBody>
+                <Row>
+                  <Col md="6">
+                    <div
+                      style={{ backgroundImage: `url(${modalFilter.userImgUrl})` }}
+                      className="modal-image"
+                    />
+                  </Col>
+                  <Col md="6">
+                    <div className="modal-title">선수 이름</div>
+                    <div className="modal-info">{modalFilter.username}</div>
+
+                    <div className="modal-title">키</div>
+                    <div className="modal-info">{modalFilter.height}cm</div>
+
+                    <div className="modal-title">포지션</div>
+                    <div className="modal-info">
+                      {modalFilter.position === 'FW' ? '공격수' : false}
+                      {modalFilter.position === 'MF' ? '미드필더' : false}
+                      {modalFilter.position === 'DF' ? '수비수' : false}
+                      {modalFilter.position === 'GK' ? '골키퍼' : false}
+                    </div>
+
+                    <div className="modal-title">주 사용발</div>
+                    <div className="modal-info">
+                      {modalFilter.foot === 'right' ? '오른발' : false}
+                      {modalFilter.foot === 'left' ? '왼발' : false}
+                      {modalFilter.foot === 'both' ? '양발 사용' : false}
+                    </div>
+                  </Col>
+                </Row>
+              </ModalBody>
+            </Modal>
+          );
+        }
+      }
+
+    );
 
 
     return (
@@ -153,7 +196,7 @@ class Ground extends Component {
                     <div
                       className="goalkeepr"
                       style={{ backgroundImage: `url(${awayGKImage})` }}
-                      id="homeGK" onClick={this.toggle}
+                      onClick={() => this.onMemberModal(awayGKUsername)}
                     />
                     <div className="player-section">{awayGKUsername}</div>
                   </div>
@@ -161,20 +204,22 @@ class Ground extends Component {
                   <div className="player2">
                     <div
                       className="away-player"
-                      style={{ backgroundImage: `url(${awayDFs0Imgge})` }}
+                      style={{ backgroundImage: `url(${awayDF1Imgge})` }}
+                      onClick={() => this.onMemberModal(awayDF1Username)}
                     />
                     <div className="player-section">
-                      {awayDFs0Username}
+                      {awayDF1Username}
                     </div>
                   </div>
 
                   <div className="player3">
                     <div
                       className="away-player"
-                      style={{ backgroundImage: `url(${awayDFs1Imgge})` }}
+                      style={{ backgroundImage: `url(${awayDF2Imgge})` }}
+                      onClick={() => this.onMemberModal(awayDF2Username)}
                     />
                     <div className="player-section">
-                      {awayDFs1Username}
+                      {awayDF2Username}
                     </div>
                   </div>
 
@@ -182,6 +227,7 @@ class Ground extends Component {
                     <div
                       className="away-player"
                       style={{ backgroundImage: `url(${awayMFImage})` }}
+                      onClick={() => this.onMemberModal(awayMFUsername)}
                     />
                     <div className="player-section">{awayMFUsername}</div>
                   </div>
@@ -190,6 +236,7 @@ class Ground extends Component {
                     <div
                       className="away-player"
                       style={{ backgroundImage: `url(${awayFWImage})` }}
+                      onClick={() => this.onMemberModal(awayFWUsername)}
                     />
                     <div className="player-section">{awayFWUsername}</div>
                   </div>
@@ -209,6 +256,7 @@ class Ground extends Component {
                     <div
                       className="goalkeepr"
                       style={{ backgroundImage: `url(${homeGKImage})` }}
+                      onClick={() => this.onMemberModal(homeGKUsername)}
                     />
                     <div className="player-section">{homeGKUsername}</div>
                   </div>
@@ -216,20 +264,22 @@ class Ground extends Component {
                   <div className="player2">
                     <div
                       className="home-player"
-                      style={{ backgroundImage: `url(${homeDFs0Imgge})` }}
+                      style={{ backgroundImage: `url(${homeDF1Imgge})` }}
+                      onClick={() => this.onMemberModal(homeDF1Username)}
                     />
                     <div className="player-section">
-                      {homeDFs0Username}
+                      {homeDF1Username}
                     </div>
                   </div>
 
                   <div className="player3">
                     <div
                       className="home-player"
-                      style={{ backgroundImage: `url(${homeDFs1Imgge})` }}
+                      style={{ backgroundImage: `url(${homeDF2Imgge})` }}
+                      onClick={() => this.onMemberModal(homeDF2Username)}
                     />
                     <div className="player-section">
-                      {homeDFs1Username}
+                      {homeDF2Username}
                     </div>
                   </div>
 
@@ -237,6 +287,7 @@ class Ground extends Component {
                     <div
                       className="home-player"
                       style={{ backgroundImage: `url(${homeMFImage})` }}
+                      onClick={() => this.onMemberModal(homeMFUsername)}
                     />
                     <div className="player-section">{homeMFUsername}</div>
                   </div>
@@ -245,6 +296,7 @@ class Ground extends Component {
                     <div
                       className="home-player"
                       style={{ backgroundImage: `url(${homeFWImage})` }}
+                      onClick={() => this.onMemberModal(homeFWUsername)}
                     />
                     <div className="player-section">{homeFWUsername}</div>
                   </div>
@@ -256,7 +308,7 @@ class Ground extends Component {
             </Col>
           </Row>
         </Container>
-        {awayPlayer1}
+        {playerModal(this.state.member)}
       </div>
     );
   }
